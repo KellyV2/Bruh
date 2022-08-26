68dd5c18a95091ec357d1b8deef272f669ba2b04
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-26 05:25:51 +0000

    Add files via upload

--- main-4-1.cpp
+++ main-4-1.cpp
@@ -5,12 +5,7 @@ extern int sum_middle_row_column(int **vals, int num_rows, int num_cols);
 using namespace std;
 
 int main() {
-    int array[3][3];
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 5; j++) {
-            array[i][j] = 1;
-        }
-    }
-    cout << sum_middle_row_column(array,3,3);
+    int array[3][3] = {{1,1,1},{1,1,1},{1,1,1}};
+    cout << sum_middle_row_column(array,3,3) << endl;
     return 0;
 }
