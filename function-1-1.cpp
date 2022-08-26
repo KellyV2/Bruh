a6759500f298a27ad800be4f4caf34eca0d597fa
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-26 06:19:22 +0000

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,5 +1,10 @@
 int **pointer_array(int *vals, int len) {
+    int** array = new int[len]
     for (int i = 0; i < len; i++) {
-        *(vals+i) = (*vals+i);
+        array[i] = new int
     }
-}
+    for (int j = 0; j < len; j++) {
+        new int = array[j];
+    }
+    return array;
+ }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -7,6 +7,6 @@ using namespace std;
 int main() {
     int vals[]={1,2,3,4,5};
     int **val_ptrs = pointer_array(vals,5);
-    *(val_ptrs[2])=10;
+    *(val_ptrs[2]) = 10;
     return 0;
 }
