75450bd75a977cb26712bb4fa939cef743017d0d
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-26 04:57:22 +0000

    Add files via upload

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,7 +1,9 @@
 int even_numbers_above(int vals[], int above, int length) {
     int count = 0;
     for (int i = 0; i < length; i++) {
-        if ()
+        if (vals[i] > above && vals[i] % 2 == 0) {
+            count++;
+        }
     }
     return count;
 }
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -5,5 +5,7 @@ extern int even_numbers_above(int vals[], int above, int length);
 using namespace std;
 
 int main() {
+    int array[5] = {1,2,3,4,5};
+    cout << even_numbers_above(array,1,5);
     return 0;
 }
