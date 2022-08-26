3a93386c8f3cf4589de075838d3b3080e1ff26cb
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-26 06:23:18 +0000

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,10 +1,10 @@
 int **pointer_array(int *vals, int len) {
-    int** array = new int[len]
+    int** array = reinterpret_cast<int **>(new int[len]);
     for (int i = 0; i < len; i++) {
-        array[i] = new int
+        array[i] = new int;
     }
     for (int j = 0; j < len; j++) {
-        new int = array[j];
+        array[j] = reinterpret_cast<int *>(vals[j]);
     }
     return array;
  }
