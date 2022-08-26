a74cad63912237bfe32f17b35ba4fd5d850a6a9b
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-26 05:50:02 +0000

    Add files via upload

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,10 +1,10 @@
 int sum_middle_row_column(int **vals, int num_rows, int num_cols) {
     int sum = 0;
     for(int col = 0; col < num_cols; col++) {
-        sum += vals[num_cols/2][col];
+        sum += vals[num_rows/2][col];
     }
     for (int row = 0; row < num_rows; row++) {
-        sum += vals[row][num_rows/2];
+        sum += vals[row][num_cols/2];
     }
     return sum;
 }
