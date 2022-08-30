decb35e23fe538756c97105cf8f3def23452960d
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-30 06:01:00 +0000

    Add files via upload

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
-
-extern int **pointer_array(int *vals, int len);
-
-using namespace std;
+#include "capybara.h"
 
 int main() {
-    int vals[]={1,2,3,4,5};
-    int **val_ptrs = pointer_array(vals,5);
-    *(val_ptrs[2]) = 10;
+
     return 0;
 }
