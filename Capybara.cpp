d1231daedf90d771695d824378c68b49ec0debb1
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-30 06:03:02 +0000

    Add files via upload

---
+++ Capybara.cpp
@@ -0,0 +1,6 @@
+#include "Capybara.h"
+
+int main() {
+
+    return 0;
+}
---
+++ Capybara.h
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class  Capybara {
+public:
+    string name;
+    int age;
+    void setName(string capyName);
+    string getName();
+    void setAge();
+    int getAge();
+};
+
+int Capybara::getName() {}
